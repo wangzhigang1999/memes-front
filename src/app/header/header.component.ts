d0b66973db66869ede77b0bcacd6f77e0a267692
Zhigang <wangzhigang1999@live.cn>
2023-08-07 23:48:44 +0800

    增加了全屏设置

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, HostListener, Inject} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,43 @@ import {Component} from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-    protected readonly window = window;
+  protected readonly window = window;
+
+  constructor(@Inject(DOCUMENT) private document: any) {
+  }
+
+  elem: any;
+  showHeader = true;
+
+  ngOnInit() {
+    this.elem = document.documentElement;
+  }
+
+  openFullscreen() {
+    if (this.elem.requestFullscreen) {
+      this.elem.requestFullscreen();
+      this.showHeader = false;
+    } else if (this.elem.mozRequestFullScreen) {
+      /* Firefox */
+      this.elem.mozRequestFullScreen();
+      this.showHeader = false;
+    } else if (this.elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.elem.webkitRequestFullscreen();
+      this.showHeader = false;
+    } else if (this.elem.msRequestFullscreen) {
+      /* IE/Edge */
+      this.elem.msRequestFullscreen();
+      this.showHeader = false;
+    }
+  }
+
+
+  // listen to fullscreenchange event
+  @HostListener('document:fullscreenchange', ['$event']) onFullScreenChange(event: any) {
+    if (!this.document.fullscreenElement) {
+      this.showHeader = true;
+    }
+  }
+
 }
