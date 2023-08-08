b3154d61b9f97ec8c21d83eba673004b9f3eadb4
Zhigang <wangzhigang1999@live.cn>
2023-08-08 10:25:21 +0800

    增加了全屏设置

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -23,18 +23,6 @@ export class HeaderComponent {
     if (this.elem.requestFullscreen) {
       this.elem.requestFullscreen();
       this.showHeader = false;
-    } else if (this.elem.mozRequestFullScreen) {
-      /* Firefox */
-      this.elem.mozRequestFullScreen();
-      this.showHeader = false;
-    } else if (this.elem.webkitRequestFullscreen) {
-      /* Chrome, Safari and Opera */
-      this.elem.webkitRequestFullscreen();
-      this.showHeader = false;
-    } else if (this.elem.msRequestFullscreen) {
-      /* IE/Edge */
-      this.elem.msRequestFullscreen();
-      this.showHeader = false;
     }
   }
 
