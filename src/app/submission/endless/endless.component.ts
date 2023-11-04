0a697ef2c79bc313ba19d1b22e7039cb0d3e68ac
Zhigang <wangzhigang1999@live.cn>
2023-11-05 01:07:11 +0800

    UI 优化 增加 cache

--- src/app/submission/endless/endless.component.ts
+++ src/app/submission/endless/endless.component.ts
@@ -1,138 +1,111 @@
-import {Component, HostListener} from '@angular/core';
+import {Component} from '@angular/core';
 import {Page} from "../../model/page";
 import {Submission} from "../../model/submission";
 import {SubmissionService} from "../../service/submission.service";
 import {authorized} from "../../utils";
 
 @Component({
-  selector: 'app-endless',
-  templateUrl: './endless.component.html',
-  styleUrls: ['./endless.component.css']
+    selector: 'app-endless',
+    templateUrl: './endless.component.html',
+    styleUrls: ['./endless.component.css']
 })
 export class EndlessComponent {
 
-  public submissions: Submission[] = [];
+    public submissions: Submission[] = [];
 
-  curElement: Set<string> = new Set<string>();
-  pageSize: number = 6;
-  pageNum: number = 1;
+    curElement: Set<string> = new Set<string>();
+    pageSize: number = 6;
+    pageNum: number = 1;
 
-  total: number = 0;
-  lastId = "";
+    total: number = 0;
+    lastId = "";
 
-  adminMode = false;
+    adminMode = false;
 
-  requesting = false
+    requesting = false
 
-  default: { submissionType: string; url: string } = {
-    url: "assets/brick.jpeg",
-    submissionType: "IMAGE",
-  }
-
-  pre: any = this.default
-  cur: any = this.default
-  next: any = this.default
-  currentIndex = -1
-
-
-  // listen key event
-  @HostListener('window:keydown', ['$event']) handleKeyDown(event: KeyboardEvent) {
-    if (event.key === 'ArrowLeft') {
-      this.toPre()
-    } else if (event.key === 'ArrowRight') {
-      this.toNext()
+    default: { submissionType: string; url: string } = {
+        url: "assets/brick.jpeg",
+        submissionType: "IMAGE",
     }
-  }
 
-  constructor(private submissionService: SubmissionService) {
-    this.init()
-    if (authorized()) {
-      this.adminMode = true
-    }
-  }
 
-  init() {
-    this.requesting = true
-    this.submissionService.getPage(this.lastId, this.pageNum, this.pageSize).subscribe(
-      (data: any) => {
-        const page: Page<Submission> = data.data
-        this.submissions = page.list
-        this.pageNum = page.pageNum
-        this.pageSize = page.pageSize
-        this.total = page.total
-
-        this.submissions.forEach(
-          (submission: Submission) => {
-            this.curElement.add(submission.id)
-          }
-        )
-        // get lastId
-        if (this.submissions.length > 0) {
-          this.lastId = this.submissions[this.submissions.length - 1].id
+    constructor(private submissionService: SubmissionService) {
+        this.init()
+        if (authorized()) {
+            this.adminMode = true
         }
-        this.requesting = false
-
-        if (this.submissions.length > 0) {
-          this.cur = this.submissions[0]
-          if (this.submissions.length > 1) {
-            this.next = this.submissions[1]
-          } else {
-            this.next = this.default
-          }
-          this.currentIndex = 0
-        }
-
-      })
-
-  }
-
-  onScroll() {
-    if (this.submissions.length >= this.total || this.requesting) {
-      return
     }
-    this.requesting = true
-    this.submissionService.getPage(this.lastId, this.pageNum + 1, this.pageSize).subscribe(
-      (data: any) => {
-        const page: Page<Submission> = data.data
-        page.list.forEach(
-          (submission: Submission) => {
-            if (!this.curElement.has(submission.id)) {
-              this.submissions.push(submission)
-              this.curElement.add(submission.id)
+
+    init() {
+        let cache = localStorage.getItem("firstPage")
+        if (cache) {
+            let data = JSON.parse(cache)
+            let now = new Date().getMilliseconds()
+            // cache for 1 minute
+            if (now - data.timestamp < 1000 * 60) {
+                let cachePage = data.page
+                this.submissions = cachePage.list
+                this.pageNum = cachePage.pageNum
+                this.pageSize = cachePage.pageSize
+                this.total = cachePage.total
+                this.submissions.forEach((submission: Submission) => this.curElement.add(submission.id))
+                if (this.submissions.length > 0) {
+                    this.lastId = this.submissions[this.submissions.length - 1].id
+                }
+                return
             }
-          }
+        }
+
+
+        this.requesting = true
+        this.submissionService.getPage(this.lastId, this.pageNum, this.pageSize).subscribe(
+            (data: any) => {
+                const page: Page<Submission> = data.data
+                this.submissions = page.list
+                this.pageNum = page.pageNum
+                this.pageSize = page.pageSize
+                this.total = page.total
+
+                this.submissions.forEach((submission: Submission) => this.curElement.add(submission.id))
+                // get lastId
+                if (this.submissions.length > 0) {
+                    this.lastId = this.submissions[this.submissions.length - 1].id
+                }
+                this.requesting = false
+
+                if (page.list.length > 0) {
+                    // cache it for offline use, key is timestamp
+                    let now = new Date().getMilliseconds()
+                    localStorage.setItem("firstPage", JSON.stringify({timestamp: now, page: page}))
+                }
+            })
+
+    }
+
+    onScroll() {
+        if (this.submissions.length >= this.total || this.requesting) {
+            return
+        }
+        this.requesting = true
+        this.submissionService.getPage(this.lastId, this.pageNum + 1, this.pageSize).subscribe(
+            (data: any) => {
+                const page: Page<Submission> = data.data
+                page.list.forEach(
+                    (submission: Submission) => {
+                        if (!this.curElement.has(submission.id)) {
+                            this.submissions.push(submission)
+                            this.curElement.add(submission.id)
+                        }
+                    }
+                )
+                this.pageNum = page.pageNum
+                this.lastId = this.submissions[this.submissions.length - 1].id
+                this.requesting = false
+            }
         )
-        this.pageNum = page.pageNum
-        this.lastId = this.submissions[this.submissions.length - 1].id
-        this.requesting = false
-      }
-    )
-  }
-
-  toPre() {
-    if (this.currentIndex <= 0) {
-      return
-    }
-    this.currentIndex -= 1
-    this.next = this.cur
-    this.cur = this.pre
-    if (this.currentIndex == 0) {
-      this.pre = this.default
-    } else {
-      this.pre = this.submissions[this.currentIndex - 1]
-    }
-  }
-
-  toNext() {
-    this.currentIndex += 1
-    if (this.currentIndex >= this.submissions.length - 3) {
-      this.onScroll()
     }
 
-    this.pre = this.cur
-    this.cur = this.next
-    this.next = this.submissions[this.currentIndex + 1]
-  }
 }
 
 
