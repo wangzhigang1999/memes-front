6d4e4a0399fa4a522642a761f5005438c1b9a5ad
Zhigang <wangzhigang1999@live.cn>
2023-11-08 22:51:22 +0800

    bug fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,92 +2,99 @@ import {Component} from '@angular/core';
 import {Packet} from "../model/packet";
 import {WSPacketType} from "../model/packet_type";
 import {environment} from "../../environments/environment";
+import {authorized} from "../utils";
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.css']
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
-    ws: WebSocket
-    notificationDom: any
-    wsUrl: string = "ws://localhost:8080/ws"
-    currentOnlineNumber: number = 0
+  ws: WebSocket
+  notificationDom: any
+  wsUrl: string = "ws://localhost:8080/ws"
+  currentOnlineNumber: number = 0
 
 
-    constructor() {
-        this.wsUrl = environment.websocket
+  constructor() {
+    this.wsUrl = environment.websocket
 
-        this.ws = new WebSocket(this.wsUrl)
-        let firstOpen = true
-        let lastNeeMemeNotifyTimestamp: number = 0
+    this.ws = new WebSocket(this.wsUrl)
+    let firstOpen = true
+    let lastNeeMemeNotifyTimestamp: number = 0
 
-        this.ws.onmessage = (event) => {
-            if (!this.notificationDom) {
-                this.notificationDom = document.getElementById("notification")
-            }
+    this.ws.onmessage = (event) => {
+      if (!this.notificationDom) {
+        this.notificationDom = document.getElementById("notification")
+      }
 
-            let data: Packet = JSON.parse(event.data)
-            let now = new Date().getTime();
-
-
-            switch (data.type) {
-                case WSPacketType.WHISPER:
-                    this.notify(data.payload)
-                    break
-                case WSPacketType.REVIEW:
-                    if (!lastNeeMemeNotifyTimestamp || (now - lastNeeMemeNotifyTimestamp > 30 * 1000)) {
-                        this.notify("有新的 meme 图啦，稍后刷新查看")
-                        lastNeeMemeNotifyTimestamp = now;
-                    }
-                    break
-                case WSPacketType.ONLINE_NUMBER:
-                    if (firstOpen) {
-                        this.notify("当前共有 " + data.payload + " 人在线")
-                        firstOpen = false
-                    } else if (data.payload > this.currentOnlineNumber) {
-                        this.notify("有新的用户上线啦, 当前共有 " + data.payload + " 人在线")
-                    }
-                    this.currentOnlineNumber = data.payload
-                    break
-            }
+      let data: Packet = JSON.parse(event.data)
+      let now = new Date().getTime();
 
 
-        }
+      switch (data.type) {
+        case WSPacketType.WHISPER:
+          this.notify(data.payload)
+          break
+        case WSPacketType.REVIEW:
+          if (!lastNeeMemeNotifyTimestamp || (now - lastNeeMemeNotifyTimestamp > 30 * 1000)) {
+            this.notify("有新的 meme 图啦，稍后刷新查看")
+            lastNeeMemeNotifyTimestamp = now;
+          }
+          break
+        case WSPacketType.ONLINE_NUMBER:
+          if (!authorized()) {
+            break
+          }
+          if (firstOpen) {
+            this.notify("当前共有 " + data.payload + " 人在线")
+            firstOpen = false
+          } else if (data.payload > this.currentOnlineNumber) {
+            this.notify("有新的用户上线啦, 当前共有 " + data.payload + " 人在线")
+          } else if (data.payload < this.currentOnlineNumber) {
+            this.notify("有用户下线啦, 当前共有 " + data.payload + " 人在线")
+          }
 
-        this.ws.onclose = (event) => {
-            this.notify("连接已断开，正在尝试重连...")
-            this.ws = new WebSocket(this.wsUrl)
-        }
+          this.currentOnlineNumber = data.payload
+          break
+      }
 
-        this.ws.onerror = (event) => {
-            this.notify(event.type + "，正在尝试重连...")
-            this.ws = new WebSocket(this.wsUrl)
-        }
 
     }
 
-
-    notify(notifyMsg: string) {
-        // add child node
-        let node = document.createElement("div")
-        node.className = "chat chat-end"
-        node.style.opacity = "1"
-        node.innerHTML = `<p class="chat-bubble chat-bubble-success">${notifyMsg}</p>`
-        this.notificationDom.appendChild(node)
-        let nodeRemoved = false
-        // fade out
-        setInterval(() => {
-            let opacity = parseFloat(node.style.opacity)
-            if (opacity <= 0) {
-                // remove node
-                if (nodeRemoved) {
-                    return
-                }
-                node.remove()
-            }
-            node.style.opacity = `${opacity - 0.02}`
-        }, 60)
+    this.ws.onclose = (event) => {
+      this.notify("连接已断开，正在尝试重连...")
+      this.ws = new WebSocket(this.wsUrl)
     }
+
+    this.ws.onerror = (event) => {
+      this.notify(event.type + "，正在尝试重连...")
+      this.ws = new WebSocket(this.wsUrl)
+    }
+
+  }
+
+
+  notify(notifyMsg: string) {
+    // add child node
+    let node = document.createElement("div")
+    node.className = "chat chat-end"
+    node.style.opacity = "1"
+    node.innerHTML = `<p class="chat-bubble chat-bubble-success">${notifyMsg}</p>`
+    this.notificationDom.appendChild(node)
+    let nodeRemoved = false
+    // fade out
+    setInterval(() => {
+      let opacity = parseFloat(node.style.opacity)
+      if (opacity <= 0) {
+        // remove node
+        if (nodeRemoved) {
+          return
+        }
+        node.remove()
+      }
+      node.style.opacity = `${opacity - 0.02}`
+    }, 60)
+  }
 }
