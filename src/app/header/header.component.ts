772247fa4093bf5a3efbb31c1b37255ba2e2a0f1
Zhigang <wangzhigang1999@live.cn>
2023-11-08 18:58:43 +0800

    bug fixed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,67 +3,74 @@ import {Packet} from "../model/packet";
 import {WSPacketType} from "../model/packet_type";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
 
 
-  num: number = 0
-  ws: WebSocket
-  notificationDom: any
+    num: number = 0
+    ws: WebSocket
+    notificationDom: any
 
-  constructor() {
-    this.ws = new WebSocket('wss://api.memes.bupt.site/ws')
-    this.ws.onmessage = (event) => {
-      if (!this.notificationDom) {
-        this.notificationDom = document.getElementById("notification")
-      }
+    lastNeeMemeNotifyTimestamp: number = 0
 
-      let data: Packet = JSON.parse(event.data)
-      let notifyMsg = ""
+    constructor() {
+        this.ws = new WebSocket('wss://api.memes.bupt.site/ws')
+        this.ws.onmessage = (event) => {
+            if (!this.notificationDom) {
+                this.notificationDom = document.getElementById("notification")
+            }
 
-      let showNotification = false
+            let data: Packet = JSON.parse(event.data)
+            let notifyMsg = ""
 
-      switch (data.type) {
-        case WSPacketType.WHISPER:
-          notifyMsg = data.payload
-          showNotification = true
-          break
-        case WSPacketType.REVIEW:
-          notifyMsg = `有新的 meme 图啦，稍后刷新查看`
-          showNotification = true
-          break
-      }
+            let showNotification = false
 
-      if (showNotification) {
-        this.notify(notifyMsg)
-      }
+            let now = new Date().getMilliseconds();
+
+            switch (data.type) {
+                case WSPacketType.WHISPER:
+                    notifyMsg = data.payload
+                    showNotification = true
+                    break
+                case WSPacketType.REVIEW:
+                    if (!this.lastNeeMemeNotifyTimestamp || (now - this.lastNeeMemeNotifyTimestamp > 10 * 1000)) {
+                        notifyMsg = `有新的 meme 图啦，稍后刷新查看`
+                        showNotification = true
+                    }
+                    this.lastNeeMemeNotifyTimestamp = now;
+                    break
+            }
+
+            if (showNotification) {
+                this.notify(notifyMsg)
+            }
+        }
+
     }
 
-  }
 
-
-  notify(notifyMsg: string) {
-    // add child node
-    let node = document.createElement("div")
-    node.className = "chat chat-end"
-    node.style.opacity = "1"
-    node.innerHTML = `<p class="chat-bubble chat-bubble-success">${notifyMsg}</p>`
-    this.notificationDom.appendChild(node)
-    let nodeRemoved = false
-    // fade out
-    setInterval(() => {
-      let opacity = parseFloat(node.style.opacity)
-      if (opacity <= 0.2) {
-        // remove node
-        if (nodeRemoved) {
-          return
-        }
-        node.remove()
-      }
-      node.style.opacity = `${opacity - 0.01}`
-    }, 100)
-  }
+    notify(notifyMsg: string) {
+        // add child node
+        let node = document.createElement("div")
+        node.className = "chat chat-end"
+        node.style.opacity = "1"
+        node.innerHTML = `<p class="chat-bubble chat-bubble-success">${notifyMsg}</p>`
+        this.notificationDom.appendChild(node)
+        let nodeRemoved = false
+        // fade out
+        setInterval(() => {
+            let opacity = parseFloat(node.style.opacity)
+            if (opacity <= 0.2) {
+                // remove node
+                if (nodeRemoved) {
+                    return
+                }
+                node.remove()
+            }
+            node.style.opacity = `${opacity - 0.01}`
+        }, 100)
+    }
 }
