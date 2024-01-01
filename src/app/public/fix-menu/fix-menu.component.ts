da7137c5e91a2723e30676a6eaea0cc8fdac7e84
Zhigang <wangzhigang1999@live.cn>
2024-01-01 23:30:06 +0800

    准备过年！

--- src/app/public/fix-menu/fix-menu.component.ts
+++ src/app/public/fix-menu/fix-menu.component.ts
@@ -1,16 +1,35 @@
-import {Component, ElementRef, HostListener} from '@angular/core';
+import {Component, ElementRef, HostListener, OnInit} from '@angular/core';
 import {scrollToTop} from "../../utils";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-fix-menu',
   templateUrl: './fix-menu.component.html',
   styleUrls: ['./fix-menu.component.css']
 })
-export class FixMenuComponent {
+export class FixMenuComponent implements OnInit {
   showMenu: boolean = false;
+  showFixedMenu = true;
+
+  blockPageList = ["submit", "share"]
 
 
-  constructor(private elementRef: ElementRef) {
+  constructor(private elementRef: ElementRef, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.handleNavigation();
+      }
+    });
+  }
+
+  handleNavigation() {
+    // 获取当前路由
+    const currentRoute = this.router.url;
+    // 根据当前路由来决定是否显示菜单
+    this.showFixedMenu = !this.blockPageList.some((page) => currentRoute.includes(page));
   }
 
   @HostListener('document:click', ['$event'])
