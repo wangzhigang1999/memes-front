b5f30cd29866925b7292711a6f1b2a4a329e0c4a
Zhigang <wangzhigang1999@live.cn>
2024-01-01 23:43:09 +0800

    准备过年！

--- src/app/public/header/header.component.ts
+++ src/app/public/header/header.component.ts
@@ -1,10 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  showHeader = true;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.handleNavigation();
+      }
+    });
+  }
+
+  handleNavigation() {
+    // if from bbs page to other page, hide header
+    this.showHeader = !this.router.url.includes("bbs");
+  }
 
 }
