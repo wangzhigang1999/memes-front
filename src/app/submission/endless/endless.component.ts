17b4ae59f27d7926a49bae8acc280c3bea9d91f5
Zhigang <wangzhigang1999@live.cn>
2025-04-05 00:56:50 +0800

    fix(submission): 修复随机推荐配置的空值问题
    - 添加对随机推荐配置的空值检查
    - 如果配置为空，设置默认值为 false
    - 优化提交列表获取逻辑，使用新变量传递随机推荐配置

--- src/app/submission/endless/endless.component.ts
+++ src/app/submission/endless/endless.component.ts
@@ -93,8 +93,12 @@ export class EndlessComponent implements OnInit {
     }
 
     this.isLoading = true
+    let random: boolean = getConfig(this.ConfigItem.randomFetching, 'BOOLEAN')
+    if (random == null) {
+      random = false
+    }
 
-    this.submissionService.listSubmissions(this.lastId, this.pageSize, '', getConfig(this.ConfigItem.randomFetching, 'BOOLEAN')).subscribe({
+    this.submissionService.listSubmissions(this.lastId, this.pageSize, '', random).subscribe({
       next: ({ data }) => {
         // 过滤掉已存在的提交
         const newSubmissions = data.filter((submission: Submission) => !this.submissionIds.has(submission.id))
